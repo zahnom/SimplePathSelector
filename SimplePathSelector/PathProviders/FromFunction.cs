7b656b07e9d6e88ed8f9b071eaae430b11d79336
Marc Zahno <marc.zahno@ypsomed.com>
2020-02-24 14:51:08 +0100

    Added some path provider classes

--- SimplePathSelector/PathProviders/FromFunction.cs
+++ SimplePathSelector/PathProviders/FromFunction.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimplePathSelector.PathProviders
+namespace SimplePathSelectorNamespace.PathProviders
 {
     public class FromFunction
     {
