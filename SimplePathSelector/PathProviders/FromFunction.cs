5b8798bad916cc76584e601e27ab84f9913e40ee
Marc Zahno <marc.zahno@ypsomed.com>
2020-02-24 14:51:08 +0100

    Added some path provider classes

---
+++ SimplePathSelector/PathProviders/FromFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePathSelector.PathProviders
+{
+    public class FromFunction
+    {
+        private Func<string> MyFunction;
+
+        public FromFunction(Func<string> function)
+        {
+            MyFunction = function;
+        }
+        public override string ToString()
+        {
+            return MyFunction();
+        }
+    }
+}
---
+++ SimplePathSelector/PathProviders/UserInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePathSelectorNamespace.PathProviders
+{
+    public class UserInput
+    {
+        private string Input;
+
+        public UserInput(string input)
+        {
+            Input = input;
+        }
+
+        public override string ToString()
+        {
+            return Input;
+        }
+    }
+}
