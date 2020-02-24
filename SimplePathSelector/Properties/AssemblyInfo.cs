db888514cd4a2a8f11465bbb29220e9171d200c6
Marc Zahno <marc.zahno@ypsomed.com>
2020-02-24 17:04:26 +0100

    Added meta info for nuget package

--- SimplePathSelector/Properties/AssemblyInfo.cs
+++ SimplePathSelector/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SimplePathSelector")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimplePathSelector")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
